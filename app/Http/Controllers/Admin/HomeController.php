68a3910170917a1fd2821733c37a385c80ed5d1c
Laravel Daily <info@laraveldaily.com>
2021-08-28 12:09:22 +0000

    QuickAdminPanel automatic commit

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -2,10 +2,30 @@
 
 namespace App\Http\Controllers\Admin;
 
+use LaravelDaily\LaravelCharts\Classes\LaravelChart;
+
 class HomeController
 {
     public function index()
     {
-        return view('home');
+        $settings1 = [
+            'chart_title'           => 'test',
+            'chart_type'            => 'pie',
+            'report_type'           => 'group_by_date',
+            'model'                 => 'App\Models\Disease',
+            'group_by_field'        => 'deleted_at',
+            'group_by_period'       => 'month',
+            'aggregate_function'    => 'count',
+            'filter_field'          => 'created_at',
+            'filter_period'         => 'month',
+            'group_by_field_format' => 'Y-m-d H:i:s',
+            'column_class'          => 'col-md-12',
+            'entries_number'        => '5',
+            'translation_key'       => 'disease',
+        ];
+
+        $chart1 = new LaravelChart($settings1);
+
+        return view('home', compact('chart1'));
     }
 }
